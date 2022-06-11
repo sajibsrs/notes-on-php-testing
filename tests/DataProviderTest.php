d49d9eb7a3e4d5fc6bfa5dd84b9b0f5f253fc284
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-11 16:03:37 +0600

    added data provider test

---
+++ tests/DataProviderTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers DataProvider
+ */
+final class DataProviderTest extends TestCase
+{
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd(int $a, int $b, int $expected): void
+    {
+        $this->assertSame($expected, $a + $b);
+    }
+
+    public function additionProvider(): array
+    {
+        return [
+            [0, 0, 0],
+            [2, 3, 5],
+            [1, 2, 3]
+        ];
+    }
+
+    /**
+     * @dataProvider subtractNamedProvider
+     */
+    public function testSubtractNamed(int $a, int $b, int $expected): void
+    {
+        $this->assertSame($expected, $a - $b);
+    }
+
+    public function subtractNamedProvider(): array
+    {
+        return [
+            '3 minus 1' => [3, 1, 2],
+            '5 minus 2' => [5, 2, 3],
+        ];
+    }
+}
