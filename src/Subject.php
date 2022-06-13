44a1d063056c2c3791c2ba6520e9ff1b93465931
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-13 18:59:08 +0600

    subject class added

---
+++ src/Subject.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+class Subject 
+{
+    /**
+     * @var Observer[]
+     */
+    protected $observers = [];
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function attach(Observer $observer)
+    {
+        $this->observers[] = $observer;
+    }
+
+    public function doSomething()
+    {
+        $this->notify('something');
+    }
+
+    public function doSomethingBad()
+    {
+        foreach ($this->observers as $observer) {
+            $observer->reportError(42, 'Something bad happened', $this);
+        }
+    }
+
+    public function notify($argument)
+    {
+        foreach ($this->observers as $observer) {
+            $observer->update($argument);
+        }
+    }
+}
