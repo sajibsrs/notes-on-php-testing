8a0e4d01b2b641e52a9b8f3426555b69030dee0f
Sajidur Rahman <sajibsrs@gmail.com>
2022-06-13 18:58:53 +0600

    observer class added

---
+++ src/Observer.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+class Observer
+{
+    public function update($argument)
+    {
+
+    }
+
+    public function reportError($errorCode, $errorMessage, Subject $subject)
+    {
+        
+    }
+}
